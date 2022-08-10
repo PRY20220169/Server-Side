5a37e91d90b6d42a21ebf526fdbd4b95e8730183
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-08-10 18:46:41 -0500

    🎉 Started Project

---
+++ src/main/java/com/tp/pry20220169/Pry20220169Application.java
@@ -0,0 +1,13 @@
+package com.tp.pry20220169;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pry20220169Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pry20220169Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/pry20220169/Pry20220169ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.pry20220169;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pry20220169ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
