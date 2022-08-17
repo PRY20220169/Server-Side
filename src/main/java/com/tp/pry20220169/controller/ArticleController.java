239104026635d7a8650d91af1f816f5513c1e14b
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-08-17 16:45:50 -0500

    Implemented ArticleController.

---
+++ src/main/java/com/tp/pry20220169/controller/ArticleController.java
@@ -0,0 +1,66 @@
+package com.tp.pry20220169.controller;
+
+import com.tp.pry20220169.domain.model.Article;
+import com.tp.pry20220169.domain.service.ArticleService;
+import com.tp.pry20220169.resource.ArticleResource;
+import com.tp.pry20220169.resource.SaveArticleResource;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "Articles", description = "Articles API")
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @GetMapping("/articles")
+    public Page<ArticleResource> getAllArticles(Pageable pageable){
+        Page<Article> articlePage = articleService.getAllArticles(pageable);
+        List<ArticleResource> resources = articlePage.getContent()
+                .stream().map(this::convertToResource)
+                .collect(Collectors.toList());
+        return new PageImpl<>(resources, pageable, resources.size());
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public ArticleResource getArticleById(@PathVariable(name = "articleId") Long articleId){
+        return convertToResource(articleService.getArticleById(articleId));
+    }
+
+    @PostMapping("/articles")
+    public ArticleResource createArticle(@Valid @RequestBody SaveArticleResource resource){
+        Article article = convertToEntity(resource);
+        return convertToResource(articleService.createArticle(article));
+    }
+
+    @PutMapping("/articles/{articleId}")
+    public ArticleResource updateArticle(@PathVariable(name = "articleId") Long articleId,
+            @Valid @RequestBody SaveArticleResource resource){
+        Article article = convertToEntity(resource);
+        return convertToResource(articleService.updateArticle(articleId, article));
+    }
+
+    @DeleteMapping("/articles/{articleId}")
+    public ResponseEntity<?> deleteArticle(@PathVariable(name = "articleId") Long articleId){
+        return articleService.deleteArticle(articleId);
+    }
+
+    private Article convertToEntity(SaveArticleResource resource) { return mapper.map(resource, Article.class); }
+    private ArticleResource convertToResource(Article entity) { return mapper.map(entity, ArticleResource.class); }
+}
