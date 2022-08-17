a242058db6fbb77a6791f01525620e37abe324b4
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-08-17 15:21:45 -0500

    Added JPA annotation and initialized Model Mapper.

--- src/main/java/com/tp/pry20220169/Pry20220169Application.java
+++ src/main/java/com/tp/pry20220169/Pry20220169Application.java
@@ -1,13 +1,19 @@
 package com.tp.pry20220169;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Pry20220169Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pry20220169Application.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() { return new ModelMapper(); }
 }
