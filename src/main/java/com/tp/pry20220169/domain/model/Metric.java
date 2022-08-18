69531de3209a86258880216ed9da4af52a08574f
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-08-18 17:27:56 -0500

    Made bibliometric a String.

--- src/main/java/com/tp/pry20220169/domain/model/Metric.java
+++ src/main/java/com/tp/pry20220169/domain/model/Metric.java
@@ -1,9 +1,6 @@
 package com.tp.pry20220169.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -17,11 +14,9 @@ public class Metric extends AuditModel{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "bibliometric_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonIgnore
-    private Bibliometric bibliometric;
+    @NotNull
+    @NotBlank
+    private String bibliometric;
 
     @NotNull
     @NotBlank
