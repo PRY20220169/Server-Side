7e1ab4e8aac71f21bb0ec6c2790f3dff3967a42b
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-08-18 17:28:08 -0500

    Made bibliometric a String.

--- src/main/java/com/tp/pry20220169/resource/MetricResource.java
+++ src/main/java/com/tp/pry20220169/resource/MetricResource.java
@@ -1,14 +1,13 @@
 package com.tp.pry20220169.resource;
 
 import com.tp.pry20220169.domain.model.AuditModel;
-import com.tp.pry20220169.domain.model.Bibliometric;
 import lombok.Data;
 
 @Data
 public class MetricResource extends AuditModel {
 
     private Long id;
-    private Bibliometric bibliometric;
+    private String bibliometric;
     private String score;
     private int year;
     private String source;
