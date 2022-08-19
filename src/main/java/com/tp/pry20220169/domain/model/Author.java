630ac18002404d2630465e3529746d8f049f1e43
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-08-19 11:57:04 -0500

    Implemented Author.
    It's still pending to implement /authors/{authorId}/metrics.

---
+++ src/main/java/com/tp/pry20220169/domain/model/Author.java
@@ -0,0 +1,40 @@
+package com.tp.pry20220169.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+public class Author extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    private String address;
+
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @ElementCollection
+    private List<String> organizations;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private List<Metric> metrics;
+}
