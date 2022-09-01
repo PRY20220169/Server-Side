bdb8953363ecef50b3be11064f6d96433bbfb7b4
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-09-01 17:54:21 -0500

    Implemented findAllByConferenceId.

--- src/main/java/com/tp/pry20220169/domain/repository/ArticleRepository.java
+++ src/main/java/com/tp/pry20220169/domain/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package com.tp.pry20220169.domain.repository;
 
 import com.tp.pry20220169.domain.model.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    // find by conference id, etc
+    Page<Article> findAllByConferenceId(Long conferenceId, Pageable pageable);
 }
