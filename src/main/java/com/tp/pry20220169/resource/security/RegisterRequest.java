bad36134651338f869acf3cc8d17712a895092c7
Diego Johnson <diegojohnson46@gmail.com>
2022-10-03 20:54:08 -0500

    fixed some errors

---
+++ src/main/java/com/tp/pry20220169/resource/security/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.tp.pry20220169.resource.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data @AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/tp/pry20220169/resource/security/RoleToUserForm.java
@@ -0,0 +1,8 @@
+package com.tp.pry20220169.resource.security;
+import lombok.Data;
+
+@Data
+public class RoleToUserForm {
+    private String username;
+    private String roleName;
+}
